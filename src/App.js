3c6dd7a9217a2d444ca4b59e90dbd9b8870c9e77
achraf touati <oneall2050@gmail.com>
2024-12-22 01:03:56 +0000

    adding tailwind css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 function App() {
   return (
     <div>
-      <header className="App-header">
-        hello world 2 !
-      </header>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
