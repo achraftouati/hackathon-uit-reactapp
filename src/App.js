3847a463d0da8d6bcbaec9d22ffd1f767019a150
achraf touati <oneall2050@gmail.com>
2024-12-22 01:15:47 +0000

    adding leafletjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix marker icon issue
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold underline mb-4">
         Hello world!
       </h1>
+      <MapContainer center={[51.505, -0.09]} zoom={13} className="w-full h-3/4">
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
